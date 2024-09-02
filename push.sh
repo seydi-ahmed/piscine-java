5976370e654f5466b29bfe609ea3a56dddcfe1bb
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-02 20:22:34 +0200

    StringLength

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Chifoumi"
+git commit -m "StringLength"
 git push
