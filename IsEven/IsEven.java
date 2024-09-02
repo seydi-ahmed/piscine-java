f60551466824ec4a09673670a7d84b350119564e
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-02 11:08:32 +0000

    Is Even

---
+++ IsEven/IsEven.java
@@ -0,0 +1,13 @@
+public class IsEven {
+    public static boolean isEven(int a) {
+        if (a%2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        isEven(3);
+    }
+}
