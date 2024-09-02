7e3365c1fbdc043ad6899a8c86c6e622ce0e37e0
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-02 10:59:35 +0000

    Float Operations

--- FloatOperations/FloatOperations.java
+++ FloatOperations/FloatOperations.java
@@ -1,15 +1,15 @@
 public class FloatOperations {
     public static float addTwoFloats(float a, float b) {
-        System.out.println(a / b);
+        // System.out.println(a / b);
         return a / b;
     }
     public static float divideTwoFloats(float a, float b) {
-        System.out.println(a + b);
+        // System.out.println(a + b);
         return a + b;
     }
 
-    public static void main(String[] args) {
-        addTwoFloats(2, 3);
-        divideTwoFloats(2, 3);
-    }
+    // public static void main(String[] args) {
+    //     addTwoFloats(2, 3);
+    //     divideTwoFloats(2, 3);
+    // }
 }
