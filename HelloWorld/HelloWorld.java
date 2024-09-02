1a00d84cdc3e36ee6d1a84e50b5e3e262c9c3e54
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-02 10:31:02 +0000

    hello world !

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static String helloWorld() {
+        System.out.println("Hello World !");
+        return "Hello World !";
     }
+
+    // public static void main(String[] args) {
+    //     helloWorld();  // Appel de la méthode helloWorld
+    // }
 }
