27e222848b95c1ce0f23dfcb7d22d22048bad8ed
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-03 15:46:20 +0000

    SortArgs

--- SortArgs/SortArgs.java
+++ SortArgs/SortArgs.java
@@ -19,13 +19,17 @@ public class SortArgs {
         }
 
         for (int i = 0; i < args.length; i++){
-            System.out.println(args[i]);
+            System.out.print(args[i]);
+            if (i != args.length -1){
+                System.out.print(" ");
+            }
         }
+        System.out.print("\n");
     }
 
-    // public static void main(String[] args) {
-    //     String[] tab = {"23", "2", "13", "3"};
+    public static void main(String[] args) {
+        String[] tab = {"23", "2", "13", "3"};
 
-    //     sort(tab);
-    // }
+        sort(tab);
+    }
 }
