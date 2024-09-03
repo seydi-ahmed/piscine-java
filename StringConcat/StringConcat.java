55f5e5b3ce52e21fee82f25e56bfa95ee90dab9c
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-03 12:27:51 +0000

    StringConcat

---
+++ StringConcat/StringConcat.java
@@ -0,0 +1,10 @@
+public class StringConcat {
+    public static String concat(String s1, String s2) {
+        // System.out.println(s1 + s2);
+        return s1+s2;
+    }
+
+    // public static void main (String[] args) {
+    //     concat("je", " vais");
+    // }
+}
--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "StringLength"
+git commit -m "StringConcat"
 git push
