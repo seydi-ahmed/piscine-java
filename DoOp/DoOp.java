16f641e38565f9430ec7da25140fbe2e484239a2
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-03 15:18:19 +0000

    DoOp

---
+++ DoOp/DoOp.java
@@ -0,0 +1,42 @@
+public class DoOp {
+    public static String operate(String[] args) {
+        if (args == null || args.length != 3) {
+            return "Error"; // Nombre incorrect d'arguments
+        }
+
+        try {
+            // Convertir les opérandes en entiers
+            int leftOperand = Integer.parseInt(args[0]);
+            int rightOperand = Integer.parseInt(args[2]);
+            String operator = args[1];
+
+            // Effectuer l'opération en fonction de l'opérateur
+            switch (operator) {
+                case "+":
+                    return String.valueOf(leftOperand + rightOperand);
+                case "-":
+                    return String.valueOf(leftOperand - rightOperand);
+                case "*":
+                    return String.valueOf(leftOperand * rightOperand);
+                case "/":
+                    if (rightOperand == 0) {
+                        return "Error"; // Division par zéro
+                    }
+                    return String.valueOf(leftOperand / rightOperand);
+                case "%":
+                    if (rightOperand == 0) {
+                        return "Error"; // Division par zéro
+                    }
+                    return String.valueOf(leftOperand % rightOperand);
+                default:
+                    return "Error"; // Opérateur inconnu
+            }
+        } catch (NumberFormatException e) {
+            return "Error"; // Erreur de format des nombres
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     operate(args)
+    // }
+}
--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "ComputeArray"
+git commit -m "DoOp"
 git push
