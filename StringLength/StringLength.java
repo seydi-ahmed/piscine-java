6bac603d875644d31e26755cb24a56a0a828a040
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-03 12:20:42 +0000

    StringLength

--- StringLength/StringLength.java
+++ StringLength/StringLength.java
@@ -1,5 +1,10 @@
 public class StringLength {
     public static int getStringLength(String s) {
+        System.out.println(s.length());
         return s.length();
     }
+
+    // public static void main (String[] args) {
+    //     getStringLength("abc");
+    // }
 }
