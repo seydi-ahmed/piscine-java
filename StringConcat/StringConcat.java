c9240fa91befdaa0cd6d17c2c5861a2b31bc3a0a
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-03 12:30:01 +0000

    StringConcat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,10 +1,7 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        // System.out.println(s1 + s2);
-        return s1+s2;
+        if (s1 == null) s1 = "";
+        if (s2 == null) s2 = "";
+        return s1 + s2;
     }
-
-    // public static void main (String[] args) {
-    //     concat("je", " vais");
-    // }
 }
