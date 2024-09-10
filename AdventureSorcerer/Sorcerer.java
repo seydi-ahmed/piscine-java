feaf7f9c61852c797852b6482517fd9ad5717cb6
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-10 22:39:52 +0000

    Adventure Sorcerer

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -1,36 +1,26 @@
-public class Sorcerer extends Character implements Healer {
-    private int healCapacity;
-
-    // Constructeur avec les paramètres name, maxHealth et healCapacity
+public class Sorcerer extends Character
+    implements Healer {
+    private final int healCapacity;
     public Sorcerer(String name, int maxHealth, int healCapacity) {
-        super(name, maxHealth);  // Appel du constructeur de Character
+        super(name, maxHealth);
         this.healCapacity = healCapacity;
     }
-
-    // Implémentation de la méthode getHealCapacity de l'interface Healer
-    @Override
     public int getHealCapacity() {
         return this.healCapacity;
     }
-
-    // Implémentation de la méthode heal de l'interface Healer
-    @Override
-    public void heal(Character character) {
-        // Ajout de la capacité de soin à la santé actuelle du personnage
-        character.currentHealth += this.healCapacity;
-        // On s'assure que la santé actuelle ne dépasse pas la santé maximale
-        if (character.currentHealth > character.getMaxHealth()) {
-            character.currentHealth = character.getMaxHealth();
-        }
+    public void heal(Character warrior) {
+        if ((this.healCapacity + warrior.getCurrentHealth()) >= warrior.getMaxHealth()) {
+            warrior.setCurrentHealth(warrior.getMaxHealth());
+        } else {
+            warrior.setCurrentHealth(warrior.getCurrentHealth() + this.healCapacity);
+        } 
     }
-
-    // Surcharge de la méthode toString pour donner une description du Sorcier
     @Override
     public String toString() {
-        if (this.currentHealth > 0) {
-            return this.getName() + " is a sorcerer with " + this.currentHealth + " HP. It can heal " + this.healCapacity + " HP.";
-        } else {
+        if (this.getCurrentHealth() == 0) {
             return this.getName() + " is a dead sorcerer. So bad, it could heal " + this.healCapacity + " HP.";
+        } else {
+            return this.getName() + " is a sorcerer with " + this.getCurrentHealth() + " HP. It can heal " + this.healCapacity + " HP.";
         }
     }
 }
