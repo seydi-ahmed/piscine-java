de91d58afac2581bade58912fc805993ef5f7144
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-10 22:56:48 +0000

    Adventure Exception

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Adventure Weapon"
+git commit -m "Adventure Exception"
 git push
