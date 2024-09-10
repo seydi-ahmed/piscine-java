7bb34cd9886ef4f5238b1c91ffad8d6f27c89d63
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-10 22:23:20 +0000

    Adventure Utils

--- AdventureUtils/Character.java
+++ AdventureUtils/Character.java
@@ -56,7 +56,7 @@ public class Character {
     // Méthode statique pour afficher le statut de tous les personnages
     public static String printStatus() {
         if (allCharacters.isEmpty()) {
-            return "------------------------------------------\nNobody's fighting right now !\n------------------------------------------\n";
+            return "------------------------------------------\nNobody's fighting right now !\n------------------------------------------";
         }
 
         StringBuilder sb = new StringBuilder();
