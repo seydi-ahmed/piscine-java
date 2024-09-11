836722c889fde80079abd7022d76187e06ab0559
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-11 22:28:10 +0000

    set equals

---
+++ SetEquals/SetEquals.java
@@ -0,0 +1,9 @@
+import java.util.Set;
+
+public class SetEquals {
+
+    // Fonction pour vérifier si deux ensembles sont égaux
+    public static boolean areSetsEqual(Set<String> set1, Set<String> set2) {
+        return set1.equals(set2);  // Utilise la méthode equals pour comparer les deux ensembles
+    }
+}
--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "set operations"
+git commit -m "set equals"
 git push
