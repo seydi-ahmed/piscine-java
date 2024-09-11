ed53470da594e2e19be8f2fd5d023150328f9a38
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-11 22:26:26 +0000

    set operations

--- SetOperations/SetOperations.java
+++ SetOperations/SetOperations.java
@@ -2,11 +2,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetOperations {
+
+    // Fonction pour l'union de deux ensembles
     public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
-        // your code here
+        Set<Integer> resultSet = new HashSet<>(set1);  // Crée une copie de set1
+        resultSet.addAll(set2);  // Ajoute tous les éléments de set2
+        return resultSet;
     }
 
+    // Fonction pour l'intersection de deux ensembles
     public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
-        // your code here
+        Set<Integer> resultSet = new HashSet<>(set1);  // Crée une copie de set1
+        resultSet.retainAll(set2);  // Garde uniquement les éléments présents dans set2
+        return resultSet;
     }
 }
--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "java structures"
+git commit -m "set operations"
 git push
