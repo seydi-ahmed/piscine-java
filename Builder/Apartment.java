b65758974c1122f8d20d4395bdbf51d6d6589639
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-18 20:03:57 +0000

    Builder

---
+++ Builder/Apartment.java
@@ -0,0 +1,35 @@
+public class Apartment implements Housing {
+    private int size;
+    private int price;
+    private int rooms;
+    private String name;
+
+    public Apartment() {
+        // Empty constructor
+    }
+
+    @Override
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Apartment{size=%d, price=%d, rooms=%d, name='%s'}", size, price, rooms, name);
+    }
+}
---
+++ Builder/House.java
@@ -0,0 +1,35 @@
+public class House implements Housing {
+    private int size;
+    private int price;
+    private int rooms;
+    private String name;
+
+    public House() {
+        // Empty constructor
+    }
+
+    @Override
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("House{size=%d, price=%d, rooms=%d, name='%s'}", size, price, rooms, name);
+    }
+}
