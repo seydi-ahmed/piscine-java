41d2f6328017250c5eb90dcc7f1c41bab2e8cc4a
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-18 18:38:13 +0000

    FormatDate

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -3,40 +3,46 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-
 public class FormatDate {
-
-    // Function to format date to "Le 22 août de l'an 2021 à 13h25m et 46s"
     public static String formatToFullText(LocalDateTime dateTime) {
         if (dateTime == null) {
-            return null; // Return null if dateTime is null
+            return null;
         }
-        DateTimeFormatter dayMonthFormatter = DateTimeFormatter.ofPattern("d MMMM", Locale.FRENCH);
-        String formattedDate = dateTime.format(dayMonthFormatter);
-        return String.format("Le %s de l'an %d à %dh%dm et %ds", 
-                             formattedDate, 
-                             dateTime.getYear(),
-                             dateTime.getHour(), 
-                             dateTime.getMinute(), 
-                             dateTime.getSecond());
+        // your code here
+        // System.out.println("====================");
+        // System.out.println(dateTime.getDayOfMonth());
+        DateTimeFormatter frenchFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm:ss", Locale.FRENCH);
+        String month = dateTime.format(frenchFormatter).split(" ")[1];
+        // System.out.println(month);
+        return String.format("Le %d %s de l'an %d à %dh%dm et %ds", dateTime.getDayOfMonth(), month, dateTime.getYear(), dateTime.getHour(), dateTime.getMinute(), dateTime.getSecond());
     }
-
-    // Function to format date to "febbraio 13 22"
     public static String formatSimple(LocalDate date) {
         if (date == null) {
-            return null; // Return null if date is null
+            return null;
         }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
-        return date.format(formatter);
-    }
 
-    // Function to format time to "16:18:56.8495847"
+        DateTimeFormatter italianFormatter = DateTimeFormatter.ofPattern("dd MMMM yy", Locale.ITALIAN);
+        String month = date.format(italianFormatter).split(" ")[1];
+        String year = date.format(italianFormatter).split(" ")[2];
+        String day = date.format(italianFormatter).split(" ")[0];
+        
+        return String.format("%s %s %s", month, day, year);
+    }
     public static String formatIso(LocalTime time) {
         if (time == null) {
-            return null; // Return null if time is null
+            return null;
         }
-        DateTimeFormatter isoTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.n");
-        return time.format(isoTimeFormatter);
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        
+        String formattedTime = time.format(timeFormatter);
+        
+        int nano = time.getNano();
+        if (nano != 0) {
+            
+            formattedTime += "." + String.format("%09d", nano).replaceFirst("^0+(?!$)", "");
+        }
+        return formattedTime;
     }
 
     public static void main(String[] args) {
