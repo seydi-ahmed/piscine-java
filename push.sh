77a14cdaec809ec5b0e9882508081c9dc388a8fc
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-20 21:24:15 +0000

    RevizFactory

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "RevizBluePrint"
+git commit -m "RevizFactory"
 git push
 
 # BreakDownUrl
