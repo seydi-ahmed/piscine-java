57b4893b207fa0c494c5d4b8e843a3834d7fc53f
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-20 21:34:30 +0000

    RevizLongestCommonPrefix

--- RevizCheck/RevizCheck.java
+++ RevizCheck/RevizCheck.java
@@ -1,21 +1,38 @@
-// Factory.java
-public class Factory {
-    public Product createProduct(String type) {
-        switch (type) {
-            case "A":
-                return new ConcreteProductA();
-            case "B":
-                return new ConcreteProductB();
-            default:
-                return null;
+public class RevizCheck {
+    public String findLongestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0){
+            return "";
         }
-    }
 
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++){
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length()-1);
+            }
+            if (prefix.isEmpty()){
+                return "";
+            }
+        }
+
+        return prefix;
+    }
 
 
 // **********************************************************************************************************************************
 
     public static void main(String[] args) {
-        System.out.println(Singleton.getInstance().whoAreYou());
+        RevizCheck lcp = new RevizCheck();
+
+        // Test case 1
+        String[] strs1 = {"flower", "flow", "flight"};
+        System.out.println("Longest common prefix: " + lcp.findLongestCommonPrefix(strs1)); // Expected output: "fl"
+
+        // Test case 2
+        String[] strs2 = {"dog", "racecar", "car"};
+        System.out.println("Longest common prefix: " + lcp.findLongestCommonPrefix(strs2)); // Expected output: ""
+
+        // Test case 3
+        String[] strs3 = {"interspecies", "interstellar", "interstate"};
+        System.out.println("Longest common prefix: " + lcp.findLongestCommonPrefix(strs3)); // Expected output: "inters"
     }
 }
--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "RevizFactory"
+git commit -m "RevizLongestCommonPrefix"
 git push
 
 # BreakDownUrl
