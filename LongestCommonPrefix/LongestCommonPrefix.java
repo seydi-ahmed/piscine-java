3148a54e023c7d09a3fe06b176316071f849cd84
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-21 08:51:50 +0000

    Long prefix

--- LongestCommonPrefix/LongestCommonPrefix.java
+++ LongestCommonPrefix/LongestCommonPrefix.java
@@ -4,16 +4,11 @@ public class LongestCommonPrefix {
             return "";
         }
 
-        // Start with the first string as the initial prefix
         String prefix = strs[0];
 
-        // Compare the prefix with each string in the array
         for (int i = 1; i < strs.length; i++) {
-            // Update the prefix by comparing with the current string
             while (strs[i].indexOf(prefix) != 0) {
-                // Reduce the prefix by one character from the end
                 prefix = prefix.substring(0, prefix.length() - 1);
-                // If the prefix becomes empty, return ""
                 if (prefix.isEmpty()) {
                     return "";
                 }
