0cb4513b35f2aa7271fd1286bd3ce8eb0a98251a
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-09-21 08:37:25 +0000

    SingleTon

--- Singleton/Singleton.java
+++ Singleton/Singleton.java
@@ -1,12 +1,9 @@
 public class Singleton {
-    // The unique instance of Singleton, initially null
     private static Singleton instance;
 
-    // Private constructor to prevent instantiation from outside
     private Singleton() {
     }
 
-    // Static method to provide global access to the Singleton instance
     public static Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton(); // Lazy initialization
@@ -14,7 +11,6 @@ public class Singleton {
         return instance;
     }
 
-    // A method to return a message, demonstrating the Singleton
     public String whoAreYou() {
         return "Hello, I am a singleton!";
     }
--- push.sh
+++ push.sh
@@ -2,8 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "ConfigProtector"
+git commit -m "SingleTon"
 git push
-
-# BreakDownUrl
-# TopFrequents
